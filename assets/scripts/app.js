4843030dc5b38caf3104b49def179aab4563c9aa
trollhero <trollhero@pop-os.localdomain>
2024-01-23 15:26:18 -0500

    Converted if statements to a swith

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -37,44 +37,88 @@ function writeToLog(ev, val, monsterHealth, playerHealth){
     inalMonsterHealh: monsterHealth,
     finalPlayerHealth: playerHealth
   };
-  if(ev === LOG_EVENT_PLAYER_ATTACK){
-    logEntry.target = "MONSTER";
+
+  switch(ev){
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = "MONSTER";
+      break; 
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry = {
+        event: ev,
+        value: val,
+        target: "MONSTER",
+        finalMonsterHealh: monsterHealth,
+        finalPlayerHealth: playerHealth
+      }
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry = {
+        event: ev,
+        value: val,
+        target: "PLAYER",
+        finalMonsterHealh: monsterHealth,
+        finalPlayerHealth: playerHealth
+      }
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry = {
+        event: ev,
+        value: val,
+        target: "PLAYER",
+        finalMonsterHealh: monsterHealth,
+        finalPlayerHealth: playerHealth
+      }
+      break;
+    case LOG_EVENT_GAME_OVER:
+      logEntry = {
+        event: ev,
+        value: val,
+        finalMonsterHealh: monsterHealth,
+        finalPlayerHealth: playerHealth
+      }
+      break;
+    default:
+      logEntry = {};
   }
-  else if(ev === LOG_EVENT_PLAYER_STRONG_ATTACK){
-    logEntry = {
-      event: ev,
-      value: val,
-      target: "MONSTER",
-      finalMonsterHealh: monsterHealth,
-      finalPlayerHealth: playerHealth
-    }
-  }
-  else if(ev === LOG_EVENT_MONSTER_ATTACK){
-    logEntry = {
-      event: ev,
-      value: val,
-      target: "PLAYER",
-      finalMonsterHealh: monsterHealth,
-      finalPlayerHealth: playerHealth
-    }
-  }
-  else if(ev === LOG_EVENT_PLAYER_HEAL){
-    logEntry = {
-      event: ev,
-      value: val,
-      target: "PLAYER",
-      finalMonsterHealh: monsterHealth,
-      finalPlayerHealth: playerHealth
-    }
-  }
-  else if(ev === LOG_EVENT_GAME_OVER){
-    logEntry = {
-      event: ev,
-      value: val,
-      finalMonsterHealh: monsterHealth,
-      finalPlayerHealth: playerHealth
-    }
-  }
+
+  // if(ev === LOG_EVENT_PLAYER_ATTACK){
+  //   logEntry.target = "MONSTER";
+  // }
+  // else if(ev === LOG_EVENT_PLAYER_STRONG_ATTACK){
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     target: "MONSTER",
+  //     finalMonsterHealh: monsterHealth,
+  //     finalPlayerHealth: playerHealth
+  //   }
+  // }
+  // else if(ev === LOG_EVENT_MONSTER_ATTACK){
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     target: "PLAYER",
+  //     finalMonsterHealh: monsterHealth,
+  //     finalPlayerHealth: playerHealth
+  //   }
+  // }
+  // else if(ev === LOG_EVENT_PLAYER_HEAL){
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     target: "PLAYER",
+  //     finalMonsterHealh: monsterHealth,
+  //     finalPlayerHealth: playerHealth
+  //   }
+  // }
+  // else if(ev === LOG_EVENT_GAME_OVER){
+  //   logEntry = {
+  //     event: ev,
+  //     value: val,
+  //     finalMonsterHealh: monsterHealth,
+  //     finalPlayerHealth: playerHealth
+  //   }
+  // }
   battleLog.push(logEntry);
 }
 
